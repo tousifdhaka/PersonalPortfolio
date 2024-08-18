f5dbfb738ad46b66af2707ef3dfe1cbc0b3c4c70
Tousif Ahsan <tousifdhaka@gmail.com>
2024-08-18 01:06:21 -0500

    deployment3

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter basename="/PersonalPortfolio">
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
+  document.getElementById('root')
 )
