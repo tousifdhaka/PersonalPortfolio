8bec0ada7fc61f7debb2f6c59bfcb198ebc21206
Tousif Ahsan <tousifdhaka@gmail.com>
2024-08-18 01:08:47 -0500

    deploy4

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx'
+import App from './App.jsx';
 
-ReactDOM.render(
-  <BrowserRouter basename="/PersonalPortfolio">
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/PersonalPortfolio">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
